88d28944563f11e1f725164b336b078da5d799fa
RomeuReif <romeureif10@gmail.com>
2025-11-02 12:23:35 -0300

    adicionado rota para projetos

--- js/spa.js
+++ js/spa.js
@@ -5,6 +5,21 @@ const routes = {
             <p>A ONG esperança viva é uma organização sem fins lucrativos dedicada a transformar vidas através da solidariedade e da inclusão social.</p>
             <img src="img/thumb_esperança-viva.jpg" alt="Equipe da ONG" width="500">
         </section>
+    `,
+    projetos: `
+        <section id="projetos">
+            <h2>Nossos Projetos</h2>
+            <div class="grid">
+                <div class="card">
+                    <h3>Projeto Sementes do Futuro</h3>
+                    <p>Educação para crianças em vulnerabilidade social.</p>
+                </div>
+                <div class="card">
+                    <h3>Projeto Mãos Solidárias</h3>
+                    <p>Distribuição de cestas básicas e roupas.</p>
+                </div>
+            </div>
+    </section>
     `
 };
 
