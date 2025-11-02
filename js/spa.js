3da190a27571661ac3384959bf5aea81ec8a0da3
RomeuReif <romeureif10@gmail.com>
2025-11-02 12:16:54 -0300

    adicionado rota para index

---
+++ js/spa.js
@@ -0,0 +1,10 @@
+const routes = {
+    home: `
+        <section id="sobre>
+            <h2>Quem somos</h2>
+            <p>A ONG esperança viva é uma organização sem fins lucrativos dedicada a transformar vidas através da solidariedade e da inclusão social.</p>
+            <img src="img/thumb_esperança-viva.jpg" alt="Equipe da ONG" width="500">
+        </section>
+    `
+};
+
